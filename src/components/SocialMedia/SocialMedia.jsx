b43bbd985a533d2776f0abef6d3548f20b39e701
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-06-02 19:29:00 +0300

    edit in social media section

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -1,49 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 import { FaLinkedin } from "react-icons/fa";
-import { BsFacebook, BsGithub } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 import { RiWhatsappFill } from "react-icons/ri";
 import { AiFillFilePdf } from "react-icons/ai";
+import { client } from "../../client";
 
 const SocialMedia = () => {
+  const [socialMedia, setSocialMedia] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "socialMedia"]';
+    client.fetch(query).then((data) => {
+      setSocialMedia(data);
+    });
+  }, []);
+  let arr = [
+    <BsGithub />,
+    <FaLinkedin />,
+    <AiFillFilePdf />,
+    <RiWhatsappFill />,
+  ];
   return (
     <div className="app__social">
-      <a
-        href="https://drive.google.com/file/d/1rHCYDhgm3X6ABHvKkF7x5-1Q8-s6g-Hn/view?usp=sharing"
-        target="_blank"
-      >
-        <div>
-          <AiFillFilePdf />
-        </div>
-      </a>
-      <a href="https://github.com/EngGemy99" target="_blank">
-        <div>
-          <BsGithub />
-        </div>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/ahmed-jamal-43b76b205/"
-        target="_blank"
-      >
-        <div>
-          <FaLinkedin />
-        </div>
-      </a>
-
-      <a href="https://wa.me/01032800043 " target="_blank">
-        <div>
-          <RiWhatsappFill />
-        </div>
-      </a>
-      {/* <a
-        href='https://www.facebook.com/dina.elorbany/
-'
-        target='_blank'
-      >
-        <div>
-          <BsFacebook />
-        </div>
-      </a> */}
+      {socialMedia.map((item, index) => {
+        return (
+          <a href={`${item.title}`} target="_blank" rel="noreferrer">
+            <div>{arr[index]}</div>
+          </a>
+        );
+      })}
     </div>
   );
 };
